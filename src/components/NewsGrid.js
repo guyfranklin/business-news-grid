d07032871537d9738d8609e65725bba5467e3004
guyfranklin <guyfranklin@gmail.com>
2025-02-06 12:12:36 +1100

    Update NewsGrid.js

--- src/components/NewsGrid.js
+++ src/components/NewsGrid.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Card, CardContent } from '../ui/card';
+import { Card, CardContent } from '../components/ui/card';
 import { ArrowUpRight, ChevronLeft, ChevronRight } from 'lucide-react';
 
 const NEWS_SOURCES = {
